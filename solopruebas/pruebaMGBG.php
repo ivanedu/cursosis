bcb0913b394bc8e901a67eb1e333b1b89721cbf4
Miluskagbg <miluskageraldine@Geraldine>
2014-07-31 16:29:38 -0500

    31/07/14-16:29-Probando tercera prueba. al mismo tiempo

--- solopruebas/pruebaMGBG.php
+++ solopruebas/pruebaMGBG.php
@@ -3,10 +3,15 @@
     function modificando()
     {
         echo "Hola MIluska modificando";
+        echo "Al mismo tiempo modificando MGBG";
     }
     function modificandox()
     {
         echo "jajajaja";
     }
     echo 'Probando desde PC';
+     function modificandoAlMismoTiempo()
+    {
+        echo "HModificando 2";
+    }
 ?>
